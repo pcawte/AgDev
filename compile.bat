91900fc60996154da6690a69052ee19e2eb5b6c2
BlastBrothers <83738994+BlastBrothers@users.noreply.github.com>
2024-07-06 19:06:40 -0400

    Enable builds from source, clean up README

---
+++ compile.bat
@@ -0,0 +1,109 @@
+setlocal
+
+set ORIGDIR="%cd%"
+set BASEDIR=%cd%\_temp
+set GITHUB=%BASEDIR%\github
+
+if not exist %BASEDIR% mkdir %BASEDIR%
+if not exist %GITHUB% mkdir %GITHUB%
+
+Rem I don't feel guilty about requiring a recent version of Powershell... see below
+if not exist %BASEDIR%\Cedev_zip powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/CE-Programming/toolchain/releases/latest/download/CEdev-Windows.zip', '%BASEDIR%\Cedev-Windows.zip')"
+if not exist %BASEDIR%\Cedev_zip powershell -Command "Invoke-WebRequest https://github.com/CE-Programming/toolchain/releases/latest/download/CEdev-Windows.zip -OutFile %BASEDIR%\Cedev-Windows.zip"
+
+cd "%BASEDIR%"
+Rem Either we use this Win10-and-up command, or tar (also Win10), or we embed VBScript... this seems best for now
+if not exist %BASEDIR%\Cedev_zip powershell -Command "Expand-Archive -Force %BASEDIR%\Cedev-Windows.zip %BASEDIR%"
+if not exist %BASEDIR%\Cedev_zip rename %BASEDIR%\CEdev CEdev_zip
+del %BASEDIR%\Cedev-Windows.zip
+
+Rem get AgDev code
+cd %GITHUB%
+if exist %GITHUB%\AgDev_git rmdir /s /q \\?\%GITHUB%\AgDev_git
+Rem was a local location for AgDev code passed to us?
+if not "%1"=="" (
+    robocopy "%1" "%GITHUB%\AgDev_git" /e
+) else (
+    git clone https://github.com/pcawte/AgDev.git AgDev_git
+)
+
+set AGDEV_GIT=%GITHUB%\AgDev_git
+
+Rem get CEdev code - using a recent stable release
+cd %GITHUB%
+set CEDEV_GIT=%GITHUB%\CEdev_git
+if not exist %CEDEV_GIT%  (
+    mkdir %CEDEV_GIT%
+    git clone https://github.com/CE-Programming/toolchain.git CEdev_git --branch v11.2
+    cd %CEDEV_GIT%
+    git switch -c tmp
+    git switch master
+    git merge tmp
+    git submodule update --init --recursive
+)
+
+Rem Duplicate CEdev repo - this will become the basis for the final build
+if exist %BASEDIR%\CEDEV_PLUS_AGDEV rmdir /s /q \\?\%BASEDIR%\CEDEV_PLUS_AGDEV
+mkdir %BASEDIR%\CEDEV_PLUS_AGDEV
+set CEDEV_PLUS_AGDEV=%BASEDIR%\CEDEV_PLUS_AGDEV
+robocopy "%CEDEV_GIT% " "%CEDEV_PLUS_AGDEV% " /e
+
+Rem Move some stuff around in /src
+Rem the /agon folder in AgDev is based on the /ce folder in CEdev, but with CE-specific stuff removed and Agon-specific stuff added
+mkdir %CEDEV_PLUS_AGDEV%\src\agon
+robocopy %CEDEV_PLUS_AGDEV%\src\ce %CEDEV_PLUS_AGDEV%\src\agon
+robocopy %CEDEV_PLUS_AGDEV%\src\ce\include %CEDEV_PLUS_AGDEV%\src\agon
+cd %CEDEV_PLUS_AGDEV%\src\agon
+del eval.src
+del getstringinput.src
+del gettokeninput.src
+del intce.src
+del tice.src
+del os_textbuffer.src
+del runprgm.src
+
+Rem now that we've copied its contents, we don't need /ce anymore
+rmdir /s /q  %CEDEV_PLUS_AGDEV%\src\ce
+
+Rem delete CEdev allocator code, since AgDev uses its own
+del "%CEDEV_PLUS_AGDEV%\src\libc\allocator.src"
+del "%CEDEV_PLUS_AGDEV%\src\libc\allocator_simple.src"
+del "%CEDEV_PLUS_AGDEV%\src\libc\allocator_standard.c"
+
+Rem delete misc .c files which had handmade asm edits made for AgDev - we will copy over these prebuilt .src files in the next step
+del "%CEDEV_PLUS_AGDEV%\src\libc\time.c"
+del "%CEDEV_PLUS_AGDEV%\src\libc\strftime.c"
+
+Rem copy over AgDev source files and build instructions
+robocopy "%AGDEV_GIT%" "%CEDEV_PLUS_AGDEV%" makefile
+robocopy "%AGDEV_GIT%\src " "%CEDEV_PLUS_AGDEV%\src" /e
+
+Rem vdp headers need to be in 2 places for some reason
+robocopy "%AGDEV_GIT%\src\agon\include\agon" "%CEDEV_PLUS_AGDEV%\src\include\agon"
+
+Rem Remove the previous build directory and make
+cd %CEDEV_PLUS_AGDEV%
+if exist %CEDEV_PLUS_AGDEV%\CEdev rmdir /s /q "%CEDEV_PLUS_AGDEV%\CEdev"
+mkdir "%CEDEV_PLUS_AGDEV%\CEdev"
+SET PATH=%PATH%;c:%BASEDIR%\CEdev_zip\bin
+Rem pause
+"%CEDEV_PLUS_AGDEV%/resources/windows/make.exe" V=1
+"%CEDEV_PLUS_AGDEV%/resources/windows/make.exe" install V=1
+
+Rem copy over .exe's from CEdev release - CEdev GitHub actions pull each .exe's repo and build, but we shouldn't need to do that.
+robocopy "%BASEDIR%\CEdev_zip\bin" "%CEDEV_PLUS_AGDEV%\CEdev\bin" /e
+
+Rem copy over AgDev example folders
+robocopy "%AGDEV_GIT%\AgExamples" "%CEDEV_PLUS_AGDEV%\CEdev\AgExamples" /e
+robocopy "%AGDEV_GIT%\sprite-demos" "%CEDEV_PLUS_AGDEV%\CEdev\sprite-demos" /e
+robocopy "%AGDEV_GIT%\tests" "%CEDEV_PLUS_AGDEV%\CEdev\tests" /e
+
+Rem copy resulting build to base directory
+if exist "%ORIGDIR%\AgDev_build" rmdir /s /q "%ORIGDIR%\AgDev_build"
+robocopy "%CEDEV_PLUS_AGDEV%\CEdev" "%ORIGDIR%\AgDev_build" /e
+
+Rem clean folders up at the end - TODO make optional
+cd "%ORIGDIR%"
+Rem rmdir "%BASEDIR%" /s /q
+
+endlocal
---
+++ makefile
@@ -0,0 +1,108 @@
+# Copyright (C) 2015-2024 CE Programming
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+DESTDIR = $(CURDIR)/
+PREFIX = CEdev
+
+include $(CURDIR)/src/common.mk
+
+LIBS := libload graphx fontlibc keypadc fileioc usbdrvce srldrvce msddrvce fatdrvce
+SRCS := crt libc libcxx agon
+TOOLS := fasmg convbin convimg convfont cedev-config
+
+ifeq ($(OS),Windows_NT)
+WINDOWS_COPY := $(call COPY,resources\windows\make.exe,$(INSTALL_BIN)) && $(call COPY,resources\windows\cedev.bat,$(INSTALL_DIR))
+else
+WINDOWS_COPY :=
+endif
+
+SRCDIR = $(call NATIVEPATH,src/$1)
+TOOLSDIR = $(call NATIVEPATH,tools/$1)
+
+all: $(TOOLS) $(SRCS) $(LIBS)
+
+$(TOOLS): check
+	$(Q)$(MAKE) -C $(call TOOLSDIR,$@)
+
+$(SRCS):
+	$(Q)$(MAKE) -C $(call SRCDIR,$@)
+
+$(LIBS): fasmg
+	$(Q)$(MAKE) -C $(call SRCDIR,$@)
+
+install: all $(addprefix install-,$(SRCS)) $(addprefix install-,$(LIBS))
+	$(Q)$(MAKE) -f linker.mk -C src -B
+	$(Q)$(call MKDIR,$(INSTALL_DIR))
+	$(Q)$(call MKDIR,$(INSTALL_BIN))
+	$(Q)$(call MKDIR,$(INSTALL_H))
+	$(Q)$(call MKDIR,$(INSTALL_TI_H))
+	$(Q)$(call MKDIR,$(INSTALL_HW_H))
+	$(Q)$(call MKDIR,$(INSTALL_LIB))
+	$(Q)$(call MKDIR,$(INSTALL_META))
+	$(Q)$(call COPY,$(call NATIVEPATH,src/makefile.mk),$(INSTALL_META))
+	$(Q)$(call COPY,$(call NATIVEPATH,src/linker_script),$(INSTALL_META))
+	$(Q)$(call COPY,$(call NATIVEPATH,tools/fasmg/fasmg-ez80/commands.alm),$(INSTALL_META))
+	$(Q)$(call COPY,$(call NATIVEPATH,tools/fasmg/fasmg-ez80/ez80.alm),$(INSTALL_META))
+	$(Q)$(call COPY,$(call NATIVEPATH,tools/fasmg/fasmg-ez80/ld.alm),$(INSTALL_META))
+	$(Q)$(call COPY,$(call NATIVEEXE,tools/convfont/convfont),$(INSTALL_BIN))
+	$(Q)$(call COPY,$(call NATIVEEXE,tools/convimg/bin/convimg),$(INSTALL_BIN))
+	$(Q)$(call COPY,$(call NATIVEEXE,tools/convbin/bin/convbin),$(INSTALL_BIN))
+	$(Q)$(call COPY,$(call NATIVEEXE,tools/cedev-config/bin/cedev-config),$(INSTALL_BIN))
+	$(Q)$(WINDOWS_COPY)
+
+$(addprefix install-,$(SRCS)): $(TOOLS)
+	$(Q)$(MAKE) -C $(call SRCDIR,$(patsubst install-%,%,$@)) install PREFIX=$(PREFIX) DESTDIR=$(DESTDIR)
+
+$(addprefix install-,$(LIBS)): $(TOOLS)
+	$(Q)$(MAKE) -C $(call SRCDIR,$(patsubst install-%,%,$@)) install PREFIX=$(PREFIX) DESTDIR=$(DESTDIR)
+
+libs: $(LIBS) $(TOOLS)
+	$(Q)$(call NATIVEEXE,tools/convbin/bin/convbin) --oformat 8xg-auto-extract \
+		$(foreach library,$(LIBS),$(addprefix --input ,$(call SRCDIR,$(library))/$(library).8xv)) --output $(call NATIVEPATH,clibs.8xg)
+
+libs-zip:
+	$(Q)$(call MKDIR,clibs)
+	$(Q)$(foreach library,$(LIBS),$(call COPY,$(call NATIVEPATH,$(call SRCDIR,$(library))/$(library).8xv),clibs) &&) $(call NATIVEEXE,7z) a clibs_separately_in_zip.zip clibs
+
+clean: $(addprefix clean-,$(TOOLS)) $(addprefix clean-,$(SRCS)) $(addprefix clean-,$(LIBS))
+	$(Q)$(call REMOVE,src/linker_script)
+	$(Q)$(call REMOVE,clibs.8xg)
+	$(Q)$(call RMDIR,docs/build)
+	$(Q)$(call RMDIR,docs/doxygen)
+
+$(addprefix clean-,$(LIBS)):
+	$(Q)$(MAKE) -C $(call SRCDIR,$(patsubst clean-%,%,$@)) clean
+
+$(addprefix clean-,$(SRCS)):
+	$(Q)$(MAKE) -C $(call SRCDIR,$(patsubst clean-%,%,$@)) clean
+
+$(addprefix clean-,$(TOOLS)):
+	$(Q)$(MAKE) -C $(call TOOLSDIR,$(patsubst clean-%,%,$@)) clean
+
+check:
+	$(Q)$(EZCC) --version || ( echo Please install ez80-clang && exit 1 )
+	$(Q)$(FASMG) $(NULL) $(NULL) || ( echo Please install fasmg && exit 1 )
+
+docs:
+	$(Q)$(MAKE) -C docs html
+
+local-docs:
+	$(Q)$(MAKE) -C docs local-html
+
+.PHONY: $(LIBS) $(SRCS)
+.PHONY: $(addprefix install-,$(SRCS)) $(addprefix install-,$(LIBS))
+.PHONY: $(addprefix clean-,$(SRCS)) $(addprefix clean-,$(LIBS))
+.PHONY: all check clean install libs docs local-docs
